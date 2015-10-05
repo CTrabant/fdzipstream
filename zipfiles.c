63238fb50c8a57fa23dbd83c9a81d4612aa0d521
CTrabant <ctrabant@gmail.com>
2015-10-04 23:58:18 -0700

    Adjustments for removal of explicit entry flushing and non-streaming ZIP archives

--- zipfiles.c
+++ zipfiles.c
@@ -21,7 +21,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * modified 2015.8.2
+ * modified 2015.10.4
  ***************************************************************************/
 
 #include <stdio.h>
@@ -44,46 +44,39 @@ int main (int argc, char *argv[])
   unsigned char *buffer = NULL;
   uint64_t bufferlength = 0;
   ssize_t writestatus;
-  
+
   int method = ZS_DEFLATE;
-  int streaming = 0;
   int fd;
   int idx;
 
   FILE *input;
   struct stat st;
-  
+
   uint64_t readcount;
   uint64_t readsize;
-  
+
   if ( argc < 2 )
     {
       fprintf (stderr, "zipfiles: write a ZIP archive to stdout containing specified files\n");
-      fprintf (stderr, "Usage: zipfiles [-s] [-0] <file1> [file2] ... > output.zip\n");
-      fprintf (stderr, "  -s  Create archive entries with streaming structures\n");
+      fprintf (stderr, "Usage: zipfiles [-0] <file1> [file2] ... > output.zip\n");
       fprintf (stderr, "  -0  Store archive entries, default is to deflate entries\n");
       fprintf (stderr, "\n");
       return 0;
     }
-  
+
   /* Set output stream to stdout */
   fd = fileno (stdout);
-  
+
   /* Initialize ZIP container */
   if ( (zstream = zs_init (fd, NULL)) == NULL )
     {
       fprintf (stderr, "Error initializing ZIP archive\n");
       return 1;
     }
-  
+
   /* Loop through input arguments and process options */
   for ( idx=1; idx < argc; idx++ )
     {
-      if ( ! strncmp (argv[idx], "-s", 2) )
-        {
-          streaming = 1;
-          continue;
-        }
       if ( ! strncmp (argv[idx], "-0", 2) )
         {
           method = ZS_STORE;
@@ -91,143 +84,79 @@ int main (int argc, char *argv[])
           continue;
         }
     }
-  
-  if ( streaming )
-    fprintf (stderr, "Creating streaming ZIP entries\n");
-  else
-    fprintf (stderr, "Creating non-streaming ZIP entries\n");
-  
+
   /* Loop through input files, skip options */
   for ( idx=1; idx < argc; idx++ )
     {
-      if ( ! strcmp (argv[idx], "-s") ||
-           ! strcmp (argv[idx], "-0") )
+      if ( ! strcmp (argv[idx], "-0") )
         continue;
-      
+
       if ( (input = fopen (argv[idx], "r")) == NULL )
         {
           fprintf (stderr, "Cannot open %s: %s\n", argv[idx], strerror(errno));
           return 1;
         }
-      
+
       if ( fstat (fileno(input), &st) )
         {
           fprintf (stderr, "Cannot stat %s: %s\n", argv[idx], strerror(errno));
           return 1;
         }
-      
-      if ( ! streaming )  /* Non-streaming, write entire entry at once */
+
+      /* Allocate buffer */
+      if ( ! buffer )
         {
-          /* Allocate buffer */
-          if ( st.st_size > bufferlength )
+          bufferlength = 1048576;
+          if ( (buffer = malloc (bufferlength)) == NULL )
             {
-              if ( (buffer = realloc (buffer, st.st_size)) == NULL )
-                {
-                  fprintf (stderr, "Cannot allocate %lld bytes\n",
-                           (long long int) st.st_size);
-                  return 1;
-                }
-              bufferlength = st.st_size;
-            }
-          
-          /* Read file into buffer */
-          readcount = 0;
-          while ( readcount < st.st_size )
-            {
-              readsize = ( (st.st_size - readcount) < MAXIMUM_READ ) ?
-                (st.st_size - readcount) : MAXIMUM_READ;
-              
-              if ( fread (buffer+readcount, readsize, 1, input) != 1 )
-                {
-                  fprintf (stderr, "Cannot read input file\n");
-                  return 1;
-                }
-              
-              readcount += readsize;
-            }
-          
-          /* Write entire entry to ZIP archive */
-          zentry = zs_writeentry (zstream, buffer, st.st_size, argv[idx],
-                                  st.st_mtime, method, &writestatus);
-          
-          if ( zentry == NULL )
-            {
-              fprintf (stderr, "Error adding %s to output ZIP (writestatus: %lld)\n",
-                       argv[idx], (long long int) writestatus);
+              fprintf (stderr, "Cannot allocate %lld bytes\n",
+                       (long long int) bufferlength);
               return 1;
             }
-          
-          fprintf (stderr, "Added %s: %lld -> %lld (%.1f%%)\n",
-                   zentry->Name,
-                   (long long int) zentry->UncompressedSize,
-                   (long long int) zentry->CompressedSize,
-                   (100.0 * zentry->CompressedSize / zentry->UncompressedSize));
         }
-      else /* Streaming, write the entry in chunks */
+
+      /* Begin ZIP entry */
+      if ( ! (zentry = zs_entrybegin (zstream, argv[idx], st.st_mtime,
+                                      method, &writestatus)) )
         {
-          /* Allocate buffer */
-          if ( ! buffer )
-            {
-              bufferlength = 1048576;
-              if ( (buffer = malloc (bufferlength)) == NULL )
-                {
-                  fprintf (stderr, "Cannot allocate %lld bytes\n",
-                           (long long int) bufferlength);
-                  return 1;
-                }
-            }
-          
-          /* Begin ZIP entry */
-          if ( ! (zentry = zs_entrybegin (zstream, argv[idx], st.st_mtime,
-                                          method, &writestatus)) )
-            {
-              fprintf (stderr, "Cannot begin ZIP entry for %s (writestatus: %lld)\n",
-                       argv[idx], (long long int) writestatus);
-              return 1;
-            }
-          
-          /* Read file into buffer */
-          readcount = 0;
-          while ( ! feof (input) )
-            {
-              readsize = fread (buffer, 1, bufferlength, input);
-              readcount += readsize;
-              
-              /* Add data to ZIP entry */
-              if ( ! zs_entrydata (zstream, zentry, buffer, readsize, 0, &writestatus) )
-                {
-                  fprintf (stderr, "Error adding entry data to ZIP for %s (writestatus: %lld)\n",
-                           argv[idx], (long long int) writestatus);
-                  return 1;
-                }
-            }
-          
-          /* Flush entry data */
-          if ( ! zs_entryflush (zstream, zentry, &writestatus) )
+          fprintf (stderr, "Cannot begin ZIP entry for %s (writestatus: %lld)\n",
+                   argv[idx], (long long int) writestatus);
+          return 1;
+        }
+
+      /* Read file into buffer */
+      readcount = 0;
+      while ( ! feof (input) )
+        {
+          readsize = fread (buffer, 1, bufferlength, input);
+          readcount += readsize;
+
+          /* Add data to ZIP entry */
+          if ( ! zs_entrydata (zstream, zentry, buffer, readsize, &writestatus) )
             {
               fprintf (stderr, "Error adding entry data to ZIP for %s (writestatus: %lld)\n",
                        argv[idx], (long long int) writestatus);
               return 1;
             }
-          
-          /* End ZIP entry */
-          if ( ! zs_entryend (zstream, zentry, &writestatus) )
-            {
-              fprintf (stderr, "Cannot end ZIP entry for %s (writestatus: %lld)\n",
-                       argv[idx], (long long int) writestatus);
-              return 1;
-            }
-          
-          fprintf (stderr, "Added %s: %lld -> %lld (%.1f%%)\n",
-                   zentry->Name,
-                   (long long int) zentry->UncompressedSize,
-                   (long long int) zentry->CompressedSize,
-                   (100.0 * zentry->CompressedSize / zentry->UncompressedSize));
         }
-      
+
+      /* End ZIP entry */
+      if ( ! zs_entryend (zstream, zentry, &writestatus) )
+        {
+          fprintf (stderr, "Cannot end ZIP entry for %s (writestatus: %lld)\n",
+                   argv[idx], (long long int) writestatus);
+          return 1;
+        }
+
+      fprintf (stderr, "Added %s: %lld -> %lld (%.1f%%)\n",
+               zentry->Name,
+               (long long int) zentry->UncompressedSize,
+               (long long int) zentry->CompressedSize,
+               (100.0 * zentry->CompressedSize / zentry->UncompressedSize));
+
       fclose (input);
-    }
-  
+    } /* Done looping over input files */
+
   /* Finish ZIP archive */
   if ( zs_finish (zstream, &writestatus) )
     {
@@ -235,15 +164,15 @@ int main (int argc, char *argv[])
                (long long int) writestatus);
       return 1;
     }
-  
+
   fprintf (stderr, "Success, created archive with %d entries\n",
            zstream->EntryCount);
-  
+
   /* Cleanup */
   zs_free (zstream);
 
   if ( buffer )
     free (buffer);
-  
+
   return 0;
 }
